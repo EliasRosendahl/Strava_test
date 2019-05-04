fcc2b4d4e559b48ce91ff524482eee72f56f1c5d
Elias <jensen.eliasrosendahl@gmail.com>
2019-05-04 17:28:27 +0200

    authorization now gives permanent access key

--- auth.py
+++ auth.py
@@ -1,10 +1,21 @@
 from stravalib.client import Client
 
 def main():
-    access_token = input("token: ")
+    user_id = input("user id: ")
     client = Client(access_token)
-    activities = client.get_activities(limit=100)
-    print(activities)
 
+    #Build url or authorizing app
+    url = client.authorization_url(client_id=user_id, redirect_uri='http://127.0.0.1:5000/authorization')
+    print(url)
+
+    #Extract access code from url response
+    access_code = input("access code: ")
+    client_secret = input("client secret: ")
+
+    #Get permanent access token
+    access_token = client.exchange_code_for_token(user_id, client_secret, access_code)
+    print(new_access_token)
+
+    
 if __name__ == "__main__":
     main()
