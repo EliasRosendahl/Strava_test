c1129d4b9c8601a2f2369050b7eec6709cd6759f
Elias <jensen.eliasrosendahl@gmail.com>
2019-05-04 18:28:17 +0200

    auth now gets read access for private map data

--- auth.py
+++ auth.py
@@ -2,10 +2,11 @@ from stravalib.client import Client
 
 def main():
     user_id = input("user id: ")
+    access_token = input("access token: ")
     client = Client(access_token)
 
     #Build url or authorizing app
-    url = client.authorization_url(client_id=user_id, redirect_uri='http://127.0.0.1:5000/authorization')
+    url = client.authorization_url(client_id=user_id, redirect_uri='http://127.0.0.1:5000/authorization', approval_prompt='auto', scope='activity:read_all')
     print(url)
 
     #Extract access code from url response
@@ -13,8 +14,8 @@ def main():
     client_secret = input("client secret: ")
 
     #Get permanent access token
-    access_token = client.exchange_code_for_token(user_id, client_secret, access_code)
-    print(new_access_token)
+    permanent_access_token = client.exchange_code_for_token(user_id, client_secret, access_code)
+    print(permanent_access_token)
 
     
 if __name__ == "__main__":
